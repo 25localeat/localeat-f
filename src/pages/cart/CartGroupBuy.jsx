edc21e2e66a780ebc0d098b7321d145ccd3bd70c
mhyeon-kim <lovelypsent@gmail.com>
2025-05-17 21:18:35 +0900

    ♻️Fix: Delete 공동구매 장바구니 중복 코드 삭제
    공동구매 장바구니 중복 코드 삭제

--- src/pages/cart/CartGroupBuy.jsx
+++ src/pages/cart/CartGroupBuy.jsx
@@ -6,7 +6,6 @@
 */
 
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './CartGeneral.css';
