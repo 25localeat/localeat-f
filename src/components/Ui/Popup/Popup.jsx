729fe29091b65854ae18cee3ce337f45e5604fc1
mhyeon-kim <0214kkkk@naver.com>
2025-06-13 23:47:26 +0900

    💄Fix: Update 장바구니 팝업 수정
    장바구니 관련된 팝업 수정 Resolves #82

--- src/components/Ui/Popup/Popup.jsx
+++ src/components/Ui/Popup/Popup.jsx
@@ -67,7 +67,7 @@ const Popup = ({ type, onConfirm, onCancel }) => {
                     <>
                         <p>감사합니다<br />주문이 완료되었습니다.</p>
                         <div className="btn-group">
-                            <button className="confirm" onClick={onConfirm}>홈으로</button>
+                            <button className="confirm" onClick={onConfirm}>주문내역으로</button>
                         </div>
                     </>
                 )}
